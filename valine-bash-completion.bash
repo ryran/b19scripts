2ef8e1018e5e72b737be6df1e93ba4e8701a0b1a
Ryan Sawhill <ryan@b19.org>
2014-08-26 15:06:38 -0400

    updated valine bash config

--- valine-bash-completion.bash
+++ valine-bash-completion.bash
@@ -1,7 +1,7 @@
 # This file is part of valine, providing intelligent valine tab-completion for BASH
 # Save it to: /etc/bash_completion.d/
 #
-# Revision date:  2014/07/25 matching up with valine 0.4.0
+# Revision date:  2014/08/26 matching up with valine 0.5.4
 # 
 # Copyright 2014 Ryan Sawhill Aroha <rsaw@redhat.com>
 # 
@@ -22,13 +22,20 @@ __v_VIRSH() {
     sudo virsh "${@}" 2>/dev/null
 }
 
-__v_getDomains() {
+__v_get_domains() {
     __v_VIRSH list --all --name
 }
 
-__v_listSnapshots() {
-    __v_VIRSH snapshot-list ${1} --name
-    source /etc/valine/${prevX2} 2>/dev/null; echo ${!isThinLv[@]}
+__v_list_snapshots() {
+    local cfgFile=/etc/valine/${1}
+    if [[ -e ${cfgFile} ]]; then
+        [[ -r ${cfgFile} ]] || return
+        local desiredColumn=$(awk '/^# Columns:/ {print $3}' ${cfgFile} | awk -F❚ '{i = 1; while ($i != "LV") { i++ }; print i}')
+        [[ ${desiredColumn} =~ [0-9]+ ]] || return
+        awk -F❚ -v desiredColumn=${desiredColumn}  '(NF == 0 || $1 ~ /^\s*($|#)/) {next}; {print $desiredColumn}' ${cfgFile}
+    else
+        __v_VIRSH snapshot-list ${1} --name
+    fi
 }
 
 _valine()  {
@@ -43,14 +50,18 @@ _valine()  {
     curr=${COMP_WORDS[COMP_CWORD]}
     prev=${COMP_WORDS[COMP_CWORD-1]}
     prevX2=${COMP_WORDS[COMP_CWORD-2]}
-
     
     # Check previous arg to see if we need to do anything special
     case "${prev}" in
         valine)
-            COMPREPLY=( $(compgen -W "-a --all $(__v_getDomains)" -- "${curr}") )
+            COMPREPLY=( $(compgen -W "-a --all $(__v_get_domains)" -- "${curr}") )
             ;;
-        --help|-h|--off|--size|list|l|start|s|shutdown|h|destroy|d|console|c)
+        --help|-h|--size|start|s|shutdown|h|destroy|d|console|c)
+            ;;
+        --off)
+            if [[ ${prevX2} =~ ^n(ew)?$ || ${COMP_WORDS[COMP_CWORD-3]} =~ ^n(ew)?$ ]]; then
+                COMPREPLY=( $(compgen -W "--size" -- "${curr}") )
+            fi
             ;;
         new|n)
             COMPREPLY=( $(compgen -W "--off --size" -- "${curr}") )
@@ -60,8 +71,8 @@ _valine()  {
                 --all|-a)
                     COMPREPLY=( $(compgen -W "--off" -- "${curr}") )  ;;
                 *)
-                    if __v_getDomains | grep -qs -- "^${prevX2}$"; then
-                        COMPREPLY=( $(compgen -W "--off $(__v_listSnapshots ${prevX2})" -- "${curr}" ) )
+                    if __v_get_domains | grep -qs -- "^${prevX2}$"; then
+                        COMPREPLY=( $(compgen -W "--off $(__v_list_snapshots ${prevX2})" -- "${curr}" ) )
                     fi
             esac
             ;;
@@ -70,17 +81,21 @@ _valine()  {
                 --all|-a)
                     : ;;
                 *)
-                    if __v_getDomains | grep -qs -- "^${prevX2}$"; then
-                        COMPREPLY=( $(compgen -W "--off $(__v_listSnapshots ${prevX2})" -- "${curr}" ) )
+                    if __v_get_domains | grep -qs -- "^${prevX2}$"; then
+                        COMPREPLY=( $(compgen -W "$(__v_list_snapshots ${prevX2})" -- "${curr}" ) )
                     fi
             esac
             ;;
         --all|-a)
-            COMPREPLY=( $(compgen -W "--off new revert start shutdown destroy" -- "${curr}") )
+            COMPREPLY=( $(compgen -W "new revert start shutdown destroy" -- "${curr}") )
             ;;
         *)
-            if __v_getDomains | grep -qs -- "^${prev}$"; then
-                COMPREPLY=( $(compgen -W "--off list new revert Delete start shutdown destroy console" -- "${curr}") )
+            if [[ ${prevX2} == --size || ${prevX2} =~ ^r(evert)?$ ]]; then
+                COMPREPLY=( $(compgen -W "--off" -- "${curr}") )
+            elif [[ ${prevX2} =~ ^n(ew)?$ ]]; then
+                COMPREPLY=( $(compgen -W "--off --size" -- "${curr}") )
+            elif __v_get_domains | grep -qs -- "^${prev}$"; then
+                COMPREPLY=( $(compgen -W "new revert Delete start shutdown destroy console" -- "${curr}") )
             fi
     esac
     return 0
